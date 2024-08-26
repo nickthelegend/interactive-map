16257a64830d0a28bb4abadf8727f97b8091d079
nickthelegend <nickthelegend@users.noreply.github.com>
2024-08-26 07:20:24 +0000

    deploy: be9bb08dc0c4469e98af57bb59220e8f9d0323fe

--- Experience/Utils/math.js
+++
@@ -1,7 +0,0 @@
-export function lerp (start, end, amount) {
-  return (1 - amount) * start + amount * end
-}
-
-export function clamp (number, min, max) {
-  return Math.max(min, Math.min(number, max))
-}
